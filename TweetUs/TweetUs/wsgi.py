dec8f3360ba70edaf07a2101bbfc9065f83c462c
Nikhilhl01 <161803080+Nikhilhl01@users.noreply.github.com>
2024-08-20 12:11:49 +0530

    Update wsgi.py

--- TweetUs/TweetUs/wsgi.py
+++ TweetUs/TweetUs/wsgi.py
@@ -16,6 +16,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TweetUs.settings')
 application = get_wsgi_application()
 
 # For hosting purposes only
-if __name__ == "__main__":
-  port = int(os.environ.get("PORT", 4000))
-  get_wsgi_application.run(host ="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#   port = int(os.environ.get("PORT", 4000))
+#   get_wsgi_application.run(host ="0.0.0.0", port=port)
